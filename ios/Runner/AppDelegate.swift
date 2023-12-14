db3e7b5e4f69d770f34081123bed92be2ea777d6
Rodrigo Sánchez <oc.rodrigosanchez@gmail.com>
2023-12-14 13:30:28 -0600

    Initial with features

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,112 @@
 import UIKit
 import Flutter
+import ConsentimientoExpreso
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    var controller : FlutterViewController?
+    let viewToPush = CEWebView()
+    var fResult : FlutterResult?
+    var uuidTrx = ""
+    var uuidClient = ""
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+        ) -> Bool {
+        
+        guard let flutterViewController  = window?.rootViewController as? FlutterViewController else {
+            return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+        }
+        
+        let flutterChannel = FlutterMethodChannel.init(name: "test_activity", binaryMessenger: flutterViewController as! FlutterBinaryMessenger);
+        flutterChannel.setMethodCallHandler { (flutterMethodCall, flutterResult) in
+            self.fResult = flutterResult
+            
+            if flutterMethodCall.method == "startNewActivity" {
+                UIView.animate(withDuration: 0.5, animations: {
+                    self.window?.rootViewController = nil
+                    self.uuidTrx = UUID().uuidString
+                    self.uuidClient = UUID().uuidString
+                    self.viewToPush.uuidTrx = self.uuidTrx
+                    self.viewToPush.uuidClient = self.uuidClient
+                    
+                    self.viewToPush.idLender = "CNVFKn77DYyZS5Du6LebjNA8IQNs2DHY"
+                    self.viewToPush.delegate = self
+                    self.viewToPush.environment = .prod
+                    self.viewToPush.isWebViewLaunched = true
+                    self.viewToPush.url = "https://app.proddicio.net/"
+                    
+                    let navigationController = UINavigationController(rootViewController: flutterViewController)
+                    
+                    self.window = UIWindow(frame: UIScreen.main.bounds)
+                    self.window?.makeKeyAndVisible()
+                    self.window.rootViewController = navigationController
+                    navigationController.isNavigationBarHidden = true
+                    navigationController.pushViewController(self.viewToPush, animated: true)
+                    
+                })
+            }
+            if flutterMethodCall.method == "getInfoDocs" {
+                if self.uuidTrx == "" || self.uuidClient == "" {
+                    self.fResult!("¡Se requiere iniciar el webview antes de consultar los datos!")
+                } else {
+                    self.viewToPush.uuidTrx = self.uuidTrx
+                    self.viewToPush.uuidClient = self.uuidClient
+                    self.viewToPush.idLender = "CNVFKn77DYyZS5Du6LebjNA8IQNs2DHY"
+                    self.viewToPush.delegate = self
+                    
+                    self.viewToPush.getDocuments()
+                }
+                
+            }
+            if flutterMethodCall.method == "getInfoData" {
+                if self.uuidTrx == "" || self.uuidClient == "" {
+                    self.fResult!("¡Se requiere iniciar el webview antes de consultar los datos!")
+                } else {
+                    self.viewToPush.uuidTrx = self.uuidTrx
+                    self.viewToPush.uuidClient = self.uuidClient
+                    self.viewToPush.idLender = "CNVFKn77DYyZS5Du6LebjNA8IQNs2DHY"
+                    self.viewToPush.delegate = self
+                    
+                    self.viewToPush.getData()
+                }
+            }
+        }
+        
+        let _ = GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    @objc func removeWebViewController() {
+        self.viewToPush.removeFromParentViewController()
+        self.viewToPush.isFlutter = true
+    }
+    
+    
 }
+
+extension AppDelegate: CEDelegate {
+    
+    func ceStatus(isFinished: Bool, step: String) {
+        self.fResult!(step)
+        if isFinished {
+            self.performSelector(onMainThread: #selector(self.removeWebViewController), with: nil, waitUntilDone: false)
+            print("Se llama a cerrar el webview")
+        }
+    }
+    
+    
+    func ceGetData(data: String) {
+//        print(data)
+//        self.performSelector(onMainThread: #selector(self.updateData(mensaje:)), with: data, waitUntilDone: false)
+        self.fResult!(data)
+    }
+    
+    func ceFiles(data: String) {
+//        self.performSelector(onMainThread: #selector(self.updateData(mensaje:)), with: data, waitUntilDone: false)
+//        print(data)
+        self.fResult!(data)
+    }
+}
+
